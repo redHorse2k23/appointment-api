40e3d25ebc18a9d0931cf504553fa52b2e055a36
psclTomulto <92087445+redHorse2k23@users.noreply.github.com>
2026-05-22 16:26:46 +0800

    added user policy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    public function canCreateAccount(User $user)
+    {
+        return $user->is_admin;
+    }
+    
+
+    public function canCreateCourt(User $user)
+    {
+        return $user->is_owner;
+    }
+
+
+}
