2eae3011b6417a3194804a652e066c2ab8e3998f
psclTomulto <92087445+redHorse2k23@users.noreply.github.com>
2026-05-22 16:28:26 +0800

    create non-user role by admin

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
+{
+  
+
+    public function createAccount(Request $request)
+    {
+        $this->authorize('canCreateAccount', User::class);
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $owner = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+        ]);
+        $owner->save();
+
+        return response()->json(['message' => 'Owner account created successfully', 'owner' => $owner], 201);
+    }
+
+}
